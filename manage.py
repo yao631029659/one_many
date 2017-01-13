84a7fb1b56dd1b4dee987470d4e5de4823a90691
yoyo3 <yao631029659@126.com>
2017-01-13 15:44:41 +0800

    one  to many 基础版

--- manage.py
+++ manage.py
@@ -1,13 +1,8 @@
 from flask.ext.script import Manager, Server
-from flask.ext.migrate import Migrate, MigrateCommand
-
 from main import app, db, User, Post
-
-migrate = Migrate(app, db)
 
 manager = Manager(app)
 manager.add_command("server", Server())
-manager.add_command('db', MigrateCommand)
 
 
 @manager.shell
